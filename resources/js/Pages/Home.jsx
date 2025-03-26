d6e85131150bc6e0d2ee2c07d2527a945076670e
yodanisesutantio <yodanise66@gmail.com>
2025-03-27 01:30:10 +0700

    Part One: Setting up the Laravel, Inertia, and ReactJS, and Installing TailwindCSS as well

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-purple-700 text-center font-bold text-6xl">
+                Hello User!
+            </h1>
+        </>
+    );
+}

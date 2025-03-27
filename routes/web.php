8ae258f7e4066b07abde215a5990bb92c634c36f
yodanisesutantio <yodanise66@gmail.com>
2025-03-27 21:49:54 +0700

    Part 6: Model Preparation

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(1.5);
-    return Inertia::render('Home', ['name' => "Mike"]);
-});
+// Route::get('/', function () {
+//     sleep(1.5);
+//     return Inertia::render('Home', ['name' => "Mike"]);
+// });
+
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class)->except('index');

d3dcb4c537705d8607c028cbbf5fe7d6f1d3f84b
yodanisesutantio <yodanise66@gmail.com>
2025-03-27 22:10:35 +0700

    Page 7: Listing Resources

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,15 +1,26 @@
 import { Link } from "@inertiajs/react";
 
-export default function Home({ name }) {
+export default function Home({ posts }) {
+    console.log(posts);
     return (
         <>
             <h1 className="text-slate-700 text-center font-bold text-6xl py-6">
-                Hello {name}
+                Hello
             </h1>
 
-            <Link preserveScroll className="block mt-[750px]" href="/">
-                {new Date().toLocaleTimeString()}
-            </Link>
+            <div className="">
+                {posts.map((post) => (
+                    <div key={post.id} className="p-4 border-b">
+                        <div className="text-sm text-slate-600">
+                            <span>Posted on: </span>
+                            <span>
+                                {new Date(post.created_at).toLocaleTimeString()}
+                            </span>
+                        </div>
+                        <p className="font-medium">{post.body}</p>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }

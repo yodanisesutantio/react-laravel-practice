99dee6069285806cf11fe20c8eec0e3e2391ae87
yodanisesutantio <yodanise66@gmail.com>
2025-03-27 21:30:36 +0700

    Part 4: Preserve Scroll

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,15 @@
+import { Link } from "@inertiajs/react";
+
 export default function Home({ name }) {
     return (
         <>
-            <h1 className="text-slate-700 text-center font-bold text-6xl">
+            <h1 className="text-slate-700 text-center font-bold text-6xl py-6">
                 Hello {name}
             </h1>
+
+            <Link preserveScroll className="block mt-[750px]" href="/">
+                {new Date().toLocaleTimeString()}
+            </Link>
         </>
     );
 }

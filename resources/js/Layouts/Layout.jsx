710304ab6f0cf11c81cb42da85927df6022b97a4
yodanisesutantio <yodanise66@gmail.com>
2025-03-27 21:23:46 +0700

    Part 3: Layouts

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,26 @@
+import { Link } from "@inertiajs/react";
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <header className="w-full py-2 px-24 bg-slate-800">
+                <nav className="w-full flex flex-row justify-between">
+                    <Link
+                        href="/"
+                        className="p-3 hover:cursor-pointer text-slate-200 hover:bg-slate-600 duration-300 rounded-lg"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        href="/create"
+                        className="p-3 hover:cursor-pointer text-slate-200 hover:bg-slate-600 duration-300 rounded-lg"
+                    >
+                        Create
+                    </Link>
+                </nav>
+            </header>
+
+            <main>{children}</main>
+        </>
+    );
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home({ name }) {
     return (
         <>
-            <h1 className="text-purple-700 text-center font-bold text-6xl">
+            <h1 className="text-slate-700 text-center font-bold text-6xl">
                 Hello {name}
             </h1>
         </>
